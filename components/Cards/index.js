04af0b1cd363832a48a7c4f799e4145010575196
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2019-12-15 18:58:14 -0800

    Almost done!

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,14 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardSelector = document.querySelector('.cards-container');
+
 axios
     .get('https://lambda-times-backend.herokuapp.com/articles')
         .then((res) => {
             const articles = res.data.articles;
-            // articles.forEach((x)=>{
+            const js = articles.javascript;
+            const bs = articles.bootstrap;
+            const jq = articles.jquery;
+            const node = articles.node;
+            const techno = articles.technology;
 
+            js.forEach((x) => {
+                cardSelector.appendChild(articleCreator(x))
+            });
+            bs.forEach((x) => {
+                cardSelector.appendChild(articleCreator(x))
+            });
+            jq.forEach((x) => {
+                cardSelector.appendChild(articleCreator(x))
+            });
+            node.forEach((x) => {
+                cardSelector.appendChild(articleCreator(x))
+            });
+            techno.forEach((x) => {
+                cardSelector.appendChild(articleCreator(x))
+            });
+            // const articleArr = res.data.article;
+            // articleArr.forEach(i => {
+            //     const articles = articleCreator(res.data);
+            //     cardSelector.appendChild(articles);
             // })
-        })
+            // const articleList = articles.map ((x) => {
+            //     return x.data.articles;
+            // })
+            // return articleList;
+
+            console.log(res);
+            // return articleArr;
+        });
+
 function articleCreator(x){
     const card = document.createElement('div');
     const headline = document.createElement('div');
@@ -37,13 +71,18 @@ function articleCreator(x){
     card.appendChild(author);
     author.appendChild(imgContainer);
     imgContainer.appendChild(authorImg);
-    imgContainer.appendChild(authorName);
+    author.appendChild(authorName);
 
     card.classList.add('card');
     headline.classList.add('headline');
     author.classList.add('author');
     imgContainer.classList.add('img-container');
 
+    headline.textContent = x.headline;
+    author.textContent = x.authorName;
+    authorImg.src = x.authorPhoto;
+
+
     return card;
 }
-console.log(articleCreator);
+

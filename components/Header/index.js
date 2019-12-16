6ba1517afc47f47ef9afb06a68885dd5deb11542
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2019-12-15 17:20:04 -0800

    Testing...

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header(obj) {
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    headerContainer.appendChild(headerDiv);
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(h1);
+    headerDiv.appendChild(temp);
+
+    date.textContent = 'SMARCH 28, 2019';
+    h1.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    return headerDiv;
+}
+headerContainer.appendChild(Header());
